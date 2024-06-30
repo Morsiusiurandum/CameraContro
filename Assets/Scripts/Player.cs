e1187c1432822e3fb6d6054b07ea24571709f79d
Zzz <Morsiusiurandum@outlook.com>
2024-06-30 22:11:13 +0800

    perf(script): optimize flow of statements

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,7 +21,11 @@ public class Player : Character
         {
             OpenCam();
         }
-        TakePhoto();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakePhoto();
+        }
     }
 
     void OpenCam()
@@ -43,34 +47,28 @@ public class Player : Character
 
     void TakePhoto()
     {
-        if (isopencarm)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                int rectwidth = Screen.width / 2;
-                int recthight = Screen.height / 2;//拍照获取屏幕的范围
-                Rect rect = new Rect(0,0,rectwidth, recthight);//矩形X坐标，矩形Y坐标，矩形的长，宽
-                RenderTexture rt = new RenderTexture(rectwidth, recthight, -1);//创建了一个渲染纹理，相机可以在这个纹理上渲染
-                playercam.targetTexture = rt;//将相机的渲染目标设置为渲染纹理
-                playercam.Render();//手动渲染一帧
-                RenderTexture.active = rt;//激活这个rt
-                Texture2D screenShot = new Texture2D(rectwidth, recthight);//创建一个2D纹理，准备保存渲染的一帧
-                screenShot.ReadPixels(rect, 0,0);//从active激活的rt中读取像素
-                screenShot.Apply();
+        if (!isopencarm) return;
 
-                playercam.targetTexture = null;
-                RenderTexture.active = null;
-                GameObject.Destroy(rt);//记得删除回收这个rt，unity不会自动回收
+        int rectwidth = Screen.width / 2;
+        int recthight = Screen.height / 2;//拍照获取屏幕的范围
+        Rect rect = new Rect(0, 0, rectwidth, recthight);//矩形X坐标，矩形Y坐标，矩形的长，宽
+        RenderTexture rt = new RenderTexture(rectwidth, recthight, -1);//创建了一个渲染纹理，相机可以在这个纹理上渲染
+        playercam.targetTexture = rt;//将相机的渲染目标设置为渲染纹理
+        playercam.Render();//手动渲染一帧
+        RenderTexture.active = rt;//激活这个rt
+        Texture2D screenShot = new Texture2D(rectwidth, recthight);//创建一个2D纹理，准备保存渲染的一帧
+        screenShot.ReadPixels(rect, 0, 0);//从active激活的rt中读取像素
+        screenShot.Apply();
 
-                byte[] bytes = screenShot.EncodeToPNG();//将2D纹理转为一个png图片
-                string filename = Application.dataPath + "/Screenshot" + photonum + ".png";
-                System.IO.File.WriteAllBytes(filename, bytes);//保存在本地
-                Debug.Log(string.Format("成功截屏了一张照片:{0}", filename));
-                photonum++;
-            }
-        }
+        playercam.targetTexture = null;
+        RenderTexture.active = null;
+        GameObject.Destroy(rt);//记得删除回收这个rt，unity不会自动回收
+
+        byte[] bytes = screenShot.EncodeToPNG();//将2D纹理转为一个png图片
+        string filename = Application.dataPath + "/Screenshot" + photonum + ".png";
+        System.IO.File.WriteAllBytes(filename, bytes);//保存在本地
+        Debug.Log($"成功截屏了一张照片:{filename}");
+        photonum++;
+
     }
-
-
-
 }
